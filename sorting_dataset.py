a54be6af920b8329e8a2fd544c5d788e69a33cc1
Abdulhakeem Abdulrahman <nicemate17@gmail.com>
2021-12-20 00:25:42 +0000

    Add variable-sized sequences

--- sorting_dataset.py
+++ sorting_dataset.py
@@ -22,9 +22,23 @@ def gen_seq(**kwargs):
     return _order
 
 
-def gen_data(N, seq_len, order_by_idx=False, low=0, high=100):
+def gen_data(N, seq_len, order_by_idx=False, low=1, high=100):
     seq_gen = gen_seq(low=low, high=high, size=(N, seq_len))
 
     X, y = seq_gen(order_by_idx=False)
     return X, y
+
+
+def gen_jagged_data(N, max_seq_len, order_by_idx=False, low=1, high=100):
+    seq_lens = np.random.randint(1, max_seq_len, size=(N) )
+    seq_lens_counts = np.unique(seq_lens, return_counts=True)
+
+    all_X = []
+    all_y = []
+    for seq_len, n in zip(*seq_lens_counts):
+        X, y = gen_data(n, seq_len, order_by_idx, low, high)
+        all_X += X.tolist()
+        all_y += y.tolist()
+
+    return all_X, all_y
 
